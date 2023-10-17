252246162d051093103476f843402f552ed20387
Henrique Luiz <122624168+henriqueluizp@users.noreply.github.com>
2023-10-17 20:04:18 -0300

    just

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { MongooseModule } from "@nestjs/mongoose";
 @Module({
   imports: [
     MongooseModule.forRoot(
-      "mongodb+srv://nerere:12345@todolist.yfj9clq.mongodb.net/?retryWrites=true&w=majority",
+      "mongodb+srv://dev:12345@cluster0.fgiue8z.mongodb.net/?retryWrites=true&w=majority",
     ),
     TasksModule,
   ],
