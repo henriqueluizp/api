a55c5adee1bb528e8432eca4972b75748951a467
Henrique Luiz <122624168+henriqueluizp@users.noreply.github.com>
2023-10-17 21:00:01 -0300

    isopicional

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,4 +1,9 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTaskDto } from './create-task.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
+export class UpdateTaskDto {
+  @IsOptional()
+  task: string;
+
+  @IsOptional()
+  complete: boolean;
+}
